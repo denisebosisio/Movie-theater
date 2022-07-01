43dcd4020bec55578504877804327be20aefba4d
denisebosisio <denisebosisio12@gmail.com>
2022-07-01 19:32:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+let comedy = [
+    {
+        title: "White Chicks",
+        director: "Keenen Ivory Wayan",
+        year: 2004,
+    },
+    {
+        title: "The Hating Game",
+        director: "Peter Hutchings",
+        year: 2021,
+    },
+    {
+        title: "Pitch Perfect",
+        director: "Jason Moore",
+        year: 2012,
+    }
+];
+console.log(comedy);
+
+let action = [
+    {
+        title: "The Hunger Games",
+        director: "Gary Ross",
+        year: 2012,
+    },
+    {
+        title: "The Fast and the Furious",
+        director: "Rob Cohen",
+        year: 2001,
+    },
+    {
+        title: "Mr. & Mrs. Smith",
+        director: "Doug Liman",
+        year: 2005,
+    }
+];
+console.log(action);
+
+let fantasy = [
+    {
+        title: "Harry Potter and the Philosopher's Stone",
+        director: "Chris Columbus",
+        year: 2001,
+    },
+    {
+        title: "The Mortal Instruments: City of Bones",
+        director: "Harald Zwart",
+        year: 2013.
+    },
+    {
+        title: "Your Name",
+        director: "Makoto Shinkai",
+        year: 2017,
+    }
+];
+console.log(fantasy);
+
+if(fantasy.title === "Your name"){
+    console.log("My favorite fantasy movie is Your name.");
+}else{
+    console.log("Few people watched the movie");
+}
